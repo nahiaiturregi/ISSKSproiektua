7aba2fed4295436c3117ffd2dfdb81e2fcddc1df
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2020-09-16 19:33:40 +0200

    phpmyadmin

--- app/index.php
+++ app/index.php
@@ -1,3 +1,14 @@
 <?php
   echo '<h1>Yeah, it works!<h1>';
-	phpinfo();
+  // phpinfo();
+  $hostname = "db";
+  $username = "admin";
+  $password = "test";
+  $db = "database";
+
+  $conn = mysqli_connect($hostname,$username,$password,$db);
+  if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }
+
+?>
