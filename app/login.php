b1e938a3bb2e52c43f91540093d7b0ad6ebde502
nahiaiturregi <ituart.n@gmail.com>
2024-09-30 18:20:22 +0200

    login aldaketak

--- app/login.php
+++ app/login.php
@@ -11,12 +11,12 @@
         die("Database connection failed: " . $conn->connect_error);
     }
 
-    if (isset($_POST['login_submit'])) {
+    if (isset($_POST['bidalita']) && $_POST['bidalita'] == '1') {
         $erabiltzailea = $_POST['erabiltzailea'];
         $pasahitza = $_POST['pasahitza'];
 
-        $erabiltzailea = mysqli_real_escape_string($conn, $erabiltzailea);
-        $pasahitza = mysqli_real_escape_string($conn, $pasahitza);
+        //$erabiltzailea = mysqli_real_escape_string($conn, $erabiltzailea);
+        //$pasahitza = mysqli_real_escape_string($conn, $pasahitza);
 
         $query = "SELECT * FROM usuarios WHERE nombre='$erabiltzailea' AND telefonoa='$pasahitza'";
         $result = mysqli_query($conn, $query);
@@ -24,7 +24,7 @@
         if (mysqli_num_rows($result) == 1) {
             $_SESSION['erabiltzailea'] = $erabiltzailea;
             echo "Login successful!";
-            // Redirect to a different page or perform other actions
+            //Redirigir
         } else {
             echo "Invalid erabiltzailea or pasahitza.";
         }
@@ -39,22 +39,24 @@
     <title>Identifikatu</title>
 </head>
 <body>
-    <form name="login_form" method="POST" action="">
+    <form name="login_form" id="login_form" method="POST" action="">
         <label>Erabiltzailea:</label><br>
         <input type="text" name="erabiltzailea"><br>
         <label>Pasahitza:</label><br>
         <input type="password" name="pasahitza"><br>
-        <input type="submit" name="login_submit" value="bidali" onclick="datuakEgiaztatu()">
+        <input type="button" name="login_submit" id="login_submit" value="bidali" onclick="datuakEgiaztatu()">
+        <input type="hidden" name="bidalita" value="0">
     </form>
     <script>
         function datuakEgiaztatu() {
             var erabiltzailea = document.login_form.erabiltzailea.value;
             var pasahitza = document.login_form.pasahitza.value;
             if (erabiltzailea == "" || pasahitza == "") {
-                alert("Erabiltzailea edo pasahitza hutsik dago");
-                return false;
+                window.alert("Erabiltzailea edo pasahitza hutsik dago");
+            } else {
+                document.login_form.bidalita.value = '1';
+                document.login_form.submit();
             }
-            return true;
         }
     </script>
 </body>
