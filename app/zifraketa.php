162413de6bbb2a39e4cf55ad41ec386d9c5257c0
Nahia Valiente <nahiav2015@gmail.com>
2024-11-18 09:15:43 +0100

    .env gakoentzako

--- app/zifraketa.php
+++ app/zifraketa.php
@@ -1,17 +1,18 @@
 <?php
-//Gakoa sortu
-define('SECRET_KEY', 'gakoa12345678901');
-define('SECRET_IV', 'iv12345678901234');
-define('SECRET_METHOD', 'AES-256-CBC');
-
 //Funtzioa enkriptatzeko
 function encrypt($data) {
-    return openssl_encrypt($data, SECRET_METHOD, SECRET_KEY, 0, SECRET_IV);
+    $method = getenv('SECRET_METHOD');
+    $key = getenv('SECRET_KEY');
+    $iv = getenv('SECRET_IV');
+    return openssl_encrypt($data, $method, $key, 0, $iv);
 }
 
 //Funtzioa dekriptatzeko
 function decrypt($data) {
-    return openssl_decrypt($data, SECRET_METHOD, SECRET_KEY, 0, SECRET_IV);
+    $method = getenv('SECRET_METHOD');
+    $key = getenv('SECRET_KEY');
+    $iv = getenv('SECRET_IV');
+    return openssl_decrypt($data, $method, $key, 0, $iv);
 }
 
 ?>
