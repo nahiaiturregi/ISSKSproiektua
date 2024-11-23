e55cb523ac944b3ddde07c1eb733690d55690317
Unai <unairc23@gmail.com>
2024-11-23 17:35:54 +0100

    No se para el programa por warnings y notices

--- app/config.php
+++ app/config.php
@@ -1,23 +1,28 @@
 <?php
-// config.php
+
 set_error_handler("customErrorHandler");
 set_exception_handler('exceptionHandler');
 register_shutdown_function('shutdownHandler');
 
 function customErrorHandler($errno, $errstr, $errfile, $errline) {
     // Mezu generikoa
+    if ($errno === E_NOTICE || $errno === E_WARNING) {
+        return true; // Ignorar y continuar la ejecución
+    }
     echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
     exit();  // Detener la ejecución del script
 }
 
 function exceptionHandler($exception) {
-    echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
+    error_log("Excepción no manejada: " . $exception->getMessage(), 3, "/var/www/html/logs/php_errors.log");
+    // echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
     exit();
 }
 
 function shutdownHandler() {
     $error = error_get_last();
     if ($error) {
-        echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
+        error_log("Error fatal: " . $error['message'], 3, "/var/www/html/logs/php_errors.log");
+        // echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
     }
 }
--- app/session_config.php
+++ app/session_config.php
@@ -1,7 +1,4 @@
 <?php
-
-include_once('config.php'); 
-
 //Saioko cookiearen parametroak ezarri eta saioa hasi
 if(session_status() == PHP_SESSION_NONE){
     session_set_cookie_params([
