4a4e9d8bbf5594903ff9ead681f35516bb48231d
Unai <unairc23@gmail.com>
2024-11-23 18:08:12 +0100

    config.php limpio

--- app/config.php
+++ app/config.php
@@ -5,24 +5,22 @@ set_exception_handler('exceptionHandler');
 register_shutdown_function('shutdownHandler');
 
 function customErrorHandler($errno, $errstr, $errfile, $errline) {
-    // Mezu generikoa
     if ($errno === E_NOTICE || $errno === E_WARNING) {
-        return true; // Ignorar y continuar la ejecución
+        return true; // Warning eta notice kontuan ez hartu
     }
+    // Mezu generikoa
     echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
-    exit();  // Detener la ejecución del script
+    exit();  
 }
 
 function exceptionHandler($exception) {
-    error_log("Excepción no manejada: " . $exception->getMessage(), 3, "/var/www/html/logs/php_errors.log");
-    // echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
+    echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
     exit();
 }
 
 function shutdownHandler() {
     $error = error_get_last();
     if ($error) {
-        error_log("Error fatal: " . $error['message'], 3, "/var/www/html/logs/php_errors.log");
-        // echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
+        echo "Errore bat gertatu da sisteman. Mesedez, saiatu beranduago.";
     }
 }
